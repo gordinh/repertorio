cdf11f91e155d748341dc799fe13b120d6a137ca
Pedro Cardoso <pedro.cardoso@cmore.pt>
2023-11-19 00:23:52 -0300

    Adding Data Analysis with Pandas course folder and making some changes to nfl_stats to clean the code

--- Web Scraping/nfl_stats.py
+++ Web Scraping/nfl_stats.py
@@ -1,63 +1,46 @@
 import requests
 from bs4 import BeautifulSoup
 from icecream import ic
-import pandas as pd
 import csv
 
 def scrape(url, year):
-  ic()
-  r = requests.get(url + year)
-  soup = BeautifulSoup(r.text, 'html.parser')
-
-  return soup.find('table', id="stats")
-
-def iterateTable(table):
-  ic()
-
-  pass
-
-footballPlayer = 'Josh Allen'
-url = 'https://www.pro-football-reference.com/players/A/AlleJo02' # understand the logic on the player name + number to automate to get multiple players
-seasonYear = ['/gamelog/2022', '/gamelog/2021', '/gamelog/2020']
-
-season_tables = [scrape(url, y) for y in seasonYear]
-
-f = open('gameData.csv', 'w', newline='')
-scrapedGames = csv.writer(f)
-
-for s in season_tables:
-  statList = []
-  headerCount = 0
-  for headers in s.find_all('th'):
-    valueHeader = headers.text
-    if valueHeader == 'Rk':
-      ic()
-      headerCount = 1
-    if headerCount == 1:
-      ic()
-      statList.append(valueHeader)
-
-  scrapedGames.writerow(statList)
-
-  gameArray = []
-  breakValue = 0
-
-  for game in s.find_all('tr'):
-    cols = game.find_all('td')
-    statList = [footballPlayer]
     ic()
+    r = requests.get(url + year)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    return soup.find('table', id="stats")
 
-    for col in cols:
-      if 'Upcoming' in col.text:
-        ic()
-        breakValue = 1
-        break
-      else:
-        ic()
-        stat_col = col.text
-        statList.append(stat_col)
-      if breakValue == 1:
-        ic()
-        break
-    scrapedGames.writerow(statList)
-    gameArray.append(statList)
+def extract_headers(table):
+    ic()
+    header_count = 0
+
+    return [header.text for header in table.find_all('th') if (header_count := header_count + 1) and header.text == 'Rk']
+
+def extract_game_data(table, player_name):
+    ic()
+    game_list = []
+
+    for game_row in table.find_all('tr'):
+        cols = game_row.find_all('td')
+        game_list.append([player_name] + [col.text for col in cols if 'Upcoming' not in col.text] )
+
+    return game_list
+
+def main():
+    football_player = 'Josh Allen'
+    url = 'https://www.pro-football-reference.com/players/A/AlleJo02'  # Update the URL logic for multiple players
+    season_years = ['/gamelog/2022', '/gamelog/2021', '/gamelog/2020']
+
+    f = open('gameData.csv', 'w', newline='')
+    scraped_games = csv.writer(f)
+
+    for year in season_years:
+        season_table = scrape(url, year)
+        if season_table:
+            headers = extract_headers(season_table)
+            scraped_games.writerow(headers)
+
+            game_data = extract_game_data(season_table, football_player)
+            [scraped_games.writerow(game) for game in game_data]
+
+if __name__ == "__main__":
+    main()
